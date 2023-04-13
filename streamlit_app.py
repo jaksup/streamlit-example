82ddbd5d12b4cc459c3b3b92d5f323c3e67919b4
jaksup <jakub.supera@gmail.com>
2023-04-13 18:44:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ with col2:
 
 with col1:
 
-    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+    m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=False, minimap_control=True)
     m.add_basemap(basemap)
     m.to_streamlit(height=700)
